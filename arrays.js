b51ced3ac984c2ac4b67af16c8681526c2bb807a
Sophie LaBelle <sophie.r.labelle@gmail.com>
2022-11-09 17:46:29 -0700

    Add a js comment on each type of method

--- arrays.js
+++ arrays.js
@@ -2,11 +2,14 @@ var studentNames = ["James", "Ellie", "Tom", "Sadie"];
 var studentsHeight = [67, 57, 75, 69];
 var studentCompleteAssignments = [true, false, true, true];
 
+// Using .pop removes the last element in a array.
 studentNames.pop();
 console.log(studentNames);
 
+// Using .push adds a new element to the end of an array.
 studentsHeight.push(47);
 console.log(studentsHeight);
 
+// Using .shift removes the first element of an array and returens its
 studentCompleteAssignments.shift();
 console.log(studentCompleteAssignments);
