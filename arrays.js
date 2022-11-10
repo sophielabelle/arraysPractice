b671bc37ce48497d0e4338a1d418aab43f761693
Sophie LaBelle <sophie.r.labelle@gmail.com>
2022-11-09 17:40:43 -0700

    Use different methods on arrays '

--- arrays.js
+++ arrays.js
@@ -1,3 +1,12 @@
 var studentNames = ["James", "Ellie", "Tom", "Sadie"];
 var studentsHeight = [67, 57, 75, 69];
-var studentCompleteAssignments = [];
+var studentCompleteAssignments = [true, false, true, true];
+
+studentNames.pop();
+console.log(studentNames);
+
+studentsHeight.push(47);
+console.log(studentsHeight);
+
+studentCompleteAssignments.shift();
+console.log(studentCompleteAssignments);
